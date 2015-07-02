dc2870626c34947bccab2cd966b32e94bcb16300
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-07-02 22:08:13 +0300

    second lesson part1

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,38 @@
+class CardsController < ApplicationController
+  def index
+  	@cards = Card.all
+  end
+
+  def show
+  	@card = Card.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	@card = Card.new(card_params)
+
+  	@card.save
+  	redirect_to @card
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to  @card
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @card = Card.find(params[:id])    
+  end
+
+private
+  def card_params
+    params.require(:card).permit(:original, :translated)
+  end
+end
