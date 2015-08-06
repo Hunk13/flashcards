05ad18ab7d340dca17dfb6c5618c51bad091907c
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-08-06 18:11:17 +0300

    fifth lesson part 1

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def index
+  end
+
+  def create
+    @card = Card.find(review_params[:card_id])
+    if @card.update_translation(review_params[:user_translation])
+      flash[:notice] = "Good!!!"
+    else
+      flash[:alert] = "Wrong!!!"
+    end
+    redirect_to reviews_path
+  end
+
+  def review_params
+    params.require(:review).permit(:card_id, :user_translation)
+  end
+end
--- app/models/card.rb
+++ app/models/card.rb
@@ -4,7 +4,20 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true, 
                                               length: { minimum: 2 }, 
                                               format: { with: /\A[A-ZА-Я]+[a-zа-я]+\z/, message: "Слова только с большой буквы" }
- 
+  before_save :set_date_after_review, on: :create
+
+  scope :for_review, -> {where("review_date <= ?", DateTime.now).order("RANDOM()")}
+
+  def set_date_after_review
+    review_date = DateTime.now + 3.days
+  end
+
+  def update_translation(user_translation)
+    if translated_text.downcase.to_s == user_translation.downcase.to_s
+      update_attribute(:review_date, review_date + 3)
+    end
+  end
+
   private
     def original_not_equal_translated
       if original_text.to_s == translated_text.to_s
