9aa782770be44343fba7e541c87260d321287675
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-08-10 15:27:40 +0300

    fifth lesson

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,23 +6,28 @@ class Card < ActiveRecord::Base
                                               format: { with: /\A[A-ZА-Я]+[a-zа-я]+\z/, message: "Слова только с большой буквы" }
   before_save :set_date_after_review, on: :create
 
-  scope :for_review, -> { where("review_date <= ?", DateTime.now).order("RANDOM()") }
+  scope :expired, -> { where("review_date <= ?", DateTime.now) }
+  scope :for_review, -> { expired.offset(rand(Card.expired.count)) }
 
   def update_translation_date(user_translation)
-    if check_words(original_text) == check_words(user_translation)
-      update_attribute(:review_date, review_date + 3.days)
+    if conversion_words(original_text) == conversion_words(user_translation)
+      update_attribute(:review_date, set_date_after_review)
     end
   end
 
   private
 
   def original_not_equal_translated
-    if check_words(original_text) == check_words(translated_text)
+    if conversion_words(original_text) == conversion_words(translated_text)
       errors.add(:original_text, "Оригинальный и переведённый тексты равны")
     end
   end
 
-  def check_words(string)
+  def conversion_words(string)
     string.mb_chars.downcase
+  end
+
+  def set_date_after_review
+    review_date = DateTime.now + 3.days
   end
 end
