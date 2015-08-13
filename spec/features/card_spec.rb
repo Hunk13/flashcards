b865a054804a2eee85194e5d2fc98aba3bc2140a
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-08-13 16:37:15 +0300

    seventh lesson sample

---
+++ spec/features/card_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "Cards to review" do
+  context "no cards to review" do
+    before(:each) do
+      visit root_path
+      click_link "Тренировка слов"
+      visit reviews_path
+    end
+
+    it "no new cards to review" do
+      expect(page).to have_content "Нет карточек для тренировки"
+     end
+  end
+
+
+  context "new cards to review" do
+    before(:each) do
+      card = create(:card)
+      Timecop.freeze(Date.today + 5.days)
+      visit root_path
+      click_link "Тренировка слов"
+      visit reviews_path
+    end
+
+    it "input right translation" do
+      fill_in "Введите перевод слова:", with: "Dog"
+      click_button "Проверить"
+      expect(page).to have_content "Правильный перевод"
+    end
+
+    it "input wrong translation" do
+      fill_in "Введите перевод:", with: "doggg"
+      click_button "Проверить"
+      expect(page).to have_content "Неправильно, попробуй еще"
+    end
+  end
+end
