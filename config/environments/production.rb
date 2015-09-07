3c95aaf3125ae937d82ed0d98ae89d0d09230887
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-09-07 17:49:00 +0300

    final tenth lesson commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   PAPERCLIP_STORAGE_OPTIONS = { storage: :s3,
-                                s3_credentials: "#{Rails.root}/config/s3.yml",
+                                access_key_id: ENV["S3_ACCESS_KEY_ID"],
+                                secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+                                bucket: ENV["S3_BUCKET"],
                                 path: "/:class/:attachment/:id_partition/:style/:filename",
                                 url: ":s3_domain_url" }
   # Settings specified here will take precedence over those in config/application.rb.
