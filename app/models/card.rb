3808ea4f0518cf34d7b1e4c8935db14c92f488f0
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-09-07 15:46:53 +0300

    add config environments

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
   has_attached_file :picture, { styles: { medium: "360x360>", thumb: "100x100>" },
-                                default_url: "/images/:style/missing.png"}.merge(PAPERCLIP_STORAGE_OPTIONS)
+                                default_url: "/images/:style/missing.png" }.merge(PAPERCLIP_STORAGE_OPTIONS)
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
