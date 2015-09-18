8d6bafec51cbb6d4cdf180423778d663646bcd50
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-09-18 08:35:34 +0300

    delete migration

--- db/migrate/20150708212123_rename_column_cardsin_table_originalto_original_text.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnCardsinTableOriginaltoOriginalText < ActiveRecord::Migration
-  def change
-    rename_column :cards, :original, :original_text
-  end
-end
--- db/migrate/20150708212325_rename_column_cardsin_table_translatedto_translated_text.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnCardsinTableTranslatedtoTranslatedText < ActiveRecord::Migration
-  def change
-    rename_column :cards, :translated, :translated_text
-  end
-end
