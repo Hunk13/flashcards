57dcd402aebeb4415b638a5448d5a007ea1754e3
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-10-15 14:50:36 +0300

    fix review.js errors

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,16 +1,16 @@
+var quality;
+
 function initTimer() {
   var interval;
-  var quality;
   if (typeof interval !== "undefined") {
     clearInterval(interval);
   }
-  interval = setInterval(function() { setQuality(); }, 1000);
+  interval = setInterval(function() { setQuality() }, 1000);
   quality = 0;
   $("#timer").text(quality);
 }
 
 function setQuality() {
-  var quality;
   quality++;
   $("#timer").text(quality);
   $("#review_quality").val(quality);
