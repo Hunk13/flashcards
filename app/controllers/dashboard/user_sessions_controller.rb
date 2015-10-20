0ef6e14815afa49bd3e6e014fee9a8f31f774def
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-10-20 14:59:47 +0300

    DRY and fix to long

--- app/controllers/dashboard/user_sessions_controller.rb
+++ app/controllers/dashboard/user_sessions_controller.rb
@@ -2,7 +2,7 @@ module Dashboard
   class UserSessionsController < BaseController
     def destroy
       logout
-      redirect_to(:home_login, notice: t("controller.user_sessions.logout"))
+      redirect_to(:home_login, notice: t("controller.user_sess.logout"))
     end
   end
 end
