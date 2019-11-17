5753782a4b6d1ec47d9fd41495f72f15186135cf
Alexandr Kalinka <alex.kalinoff@gmail.com>
2019-11-17 16:06:55 +0300

    update gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,30 @@
-require 'simplecov'
-SimpleCov.start
-
 require 'capybara/rspec'
 require 'factory_girl_rails'
 require 'email_spec'
+# require 'capybara/rails'
+require 'action_mailer'
+require 'email_spec'
+
+ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.start
+
+require File.expand_path("../../config/environment", __FILE__)
+
+require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*rb')].each { |f| require f }
+ActiveRecord::Migration.maintain_test_schema!
+
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 RSpec.configure do |config|
   config.include(EmailSpec::Helpers)
   config.include(EmailSpec::Matchers)
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
